909de367696ba21c969743134119cdbb72e0279f
t-timda <jung020623@g.hongik.ac.kr>
2026-03-22 17:13:07 +0900

    [FEAT] 스크롤바 구현 + 영화카드 컴포넌트화

--- week2/netflix/src/pages/MovieList.jsx
+++ week2/netflix/src/pages/MovieList.jsx
@@ -1,3 +1,4 @@
+{/*
 import movieData from '../data/movie.json';
 
 const MovieList = () => {
@@ -24,3 +25,29 @@ const MovieList = () => {
 }
 
 export default MovieList
+*/}
+
+import React from 'react';
+import movieData from '../data/movie.json';
+import MovieCard from '../components/MovieCard';
+
+const MovieList = () => {
+  return (
+    <main className="gap-5 p-10">
+      {/* 현재는 movie.json이 단일 객체라면 아래와 같이 전달하고, 
+        만약 배열이라면 movieData.map(movie => <MovieCard key={movie.id} movie={movie} />) 형태로 쓸 수 있습니다.
+      */}
+      {/* 가로 스크롤 컨테이너 */}
+      <div className="flex gap-6 overflow-x-auto">
+        {movieData.map((movie) => (
+          // 카드가 줄어들지 않도록 flex-shrink-0을 주고, 너비를 고정합니다.
+          <div key={movie.movieId} >
+            <MovieCard movie={movie} />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+};
+
+export default MovieList;
